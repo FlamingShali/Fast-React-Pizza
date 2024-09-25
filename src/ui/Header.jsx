06eea1c066e1ef92a521c63d91520965209a3fd5
Michał Klepner <michal.klepner1@o2.pl>
2024-09-25 11:48:23 +0200

    styling text with tailiwind

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,16 +1,18 @@
-import { Link } from "react-router-dom";
-import SearchOrder from "../features/order/SearchOrder";
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
+import Username from '../features/user/Username';
 
 const Header = () => {
   return (
-    <header>
-      <Link to='/'>Fast React Pizza Co.</Link>
+    <header className="border-b border-stone-500 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
 
       <SearchOrder />
-
-      <p>Michael</p>
+      <Username />
     </header>
-  )
+  );
 };
 
-export default Header
+export default Header;
