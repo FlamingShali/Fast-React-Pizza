f547448e07ff00165f40533731b73399319ddb9a
Michał Klepner <michal.klepner1@o2.pl>
2024-09-26 08:46:42 +0200

    styling loader component

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,10 +1,9 @@
-
 const Loader = () => {
   return (
-    <div className="loader">
-      
+    <div className="absolute inset-0 bg-slate-200/20 backdrop-blur-sm flex items-center justify-center">
+      <div className="loader"></div>
     </div>
-  )
+  );
 };
 
-export default Loader
+export default Loader;
