68c6ad315add2414be33bfba797ef44a0bf025ed
Michał Klepner <michal.klepner1@o2.pl>
2024-09-27 15:08:44 +0200

    Added quantity control funcionality

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -4,6 +4,7 @@ import { formatCurrency } from '../../utils/helpers';
 import { addItem } from '../cart/cartSlice';
 import DeleteItem from '../cart/DeleteItem';
 import { getCurrentQuantityById } from '../cart/cartSlice';
+import UpdateItemQuantity from './../cart/UpdateItemQuantity';
 
 function MenuItem({ pizza }) {
   const dispatch = useDispatch();
@@ -45,7 +46,15 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
-          {isInCart && <DeleteItem pizzaId={id} />}
+          {isInCart && (
+            <div className='flex items-center gap-3 sm:gap-8'>
+              <UpdateItemQuantity
+                pizzaId={id}
+                currentQuantity={currentQuantity}
+              />
+              <DeleteItem pizzaId={id} />{' '}
+            </div>
+          )}
 
           {!soldOut && !isInCart && (
             <Button onClick={handleAddToCart} type="small">
